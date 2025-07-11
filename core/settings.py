73bfd4e75e3216c09a27f6b054d2d4773ea5e1a8
Praze-hub <godspraiseokechukwu07@gmail.com>
2025-07-11 17:27:05 +0100

    Fixing deployment issues(2)

--- core/settings.py
+++ core/settings.py
@@ -17,8 +17,7 @@ SECRET_KEY = env("SECRET_KEY")
 
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'hustle-api.onrender.com']
 
 # Application definition
 
